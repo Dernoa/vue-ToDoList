65b55936ed5679ba0624b5e74271fcb41c19bb63
Dernoa <dernoa@zohomail.com>
2025-11-24 22:59:59 +0300

    Refactor task lists to use computed properties
    Replaced repeated filtering in v-for loops with computed properties for each task status (todo, in-progress, done, cancelled). This improves code readability and performance by avoiding redundant filtering during rendering.

--- src/components/todoView.vue
+++ src/components/todoView.vue
@@ -2,75 +2,67 @@
     <div class="taskContainer">
         <div>
             <h2>ToDo:</h2>
-            <div v-for="task in taskStore.tasks.values()" :key="task.id">
-                <div v-if="task.status == 'todo'">
-                    <div class="taskContainerInfo">
+            <div v-for="task in tasksToDo" :key="task.id">
+                <div class="taskContainerInfo">
+                    <div>
                         <div>
-                            <div>
-                                <h2>{{ task.title }}</h2>
-                            </div>
-                            <div>
-                                {{ task.description }}
-                            </div>
+                            <h2>{{ task.title }}</h2>
                         </div>
-                        <div class="taskBtn">
-                            <my-button @click="switchStatus(task.id,1)">Start work</my-button>
+                        <div>
+                            {{ task.description }}
                         </div>
+                    </div>
+                    <div class="taskBtn">
+                        <my-button @click="switchStatus(task.id,1)">Start work</my-button>
                     </div>
                 </div>
             </div>
             <h2>In-progress:</h2>
-            <div v-for="task in taskStore.tasks.values()" :key="task.id">
-                <div v-if="task.status == 'in-progress'">
-                    <div class="taskContainerInfo">
+            <div v-for="task in tasksInProgress" :key="task.id">
+                <div class="taskContainerInfo">
+                    <div>
                         <div>
-                            <div>
-                                <h2>{{ task.title }}</h2>
-                            </div>
-                            <div>
-                                {{ task.description }}
-                            </div>
+                            <h2>{{ task.title }}</h2>
                         </div>
-                        <div class="taskBtn">
-                            <my-button @click="switchStatus(task.id,2)">Done</my-button>
-                            <my-button @click="switchStatus(task.id,3)">Cancell</my-button>
+                        <div>
+                            {{ task.description }}
                         </div>
+                    </div>
+                    <div class="taskBtn">
+                        <my-button @click="switchStatus(task.id,2)">Done</my-button>
+                        <my-button @click="switchStatus(task.id,3)">Cancell</my-button>
                     </div>
                 </div>
             </div>
             <h2>Done:</h2>
-            <div v-for="task in taskStore.tasks.values()" :key="task.id">
-                <div v-if="task.status == 'done'">
-                    <div class="taskContainerInfo">
+            <div v-for="task in tasksDone" :key="task.id">
+                <div class="taskContainerInfo">
+                    <div>
                         <div>
-                            <div>
-                                <h2>{{ task.title }}</h2>
-                            </div>
-                            <div>
-                                {{ task.description }}
-                            </div>
+                            <h2>{{ task.title }}</h2>
                         </div>
-                        <div class="taskBtn">
-                            <my-button @click="removeTask(task.id)">Delete Task</my-button>
+                        <div>
+                            {{ task.description }}
                         </div>
+                    </div>
+                    <div class="taskBtn">
+                        <my-button @click="removeTask(task.id)">Delete Task</my-button>
                     </div>
                 </div>
             </div>
             <h2>Cancelled:</h2>
-            <div v-for="task in taskStore.tasks.values()" :key="task.id">
-                <div v-if="task.status == 'cancelled'">
-                    <div class="taskContainerInfo">
+            <div v-for="task in tasksCancelled" :key="task.id">
+                <div class="taskContainerInfo">
+                    <div>
                         <div>
-                            <div>
-                                <h2>{{ task.title }}</h2>
-                            </div>
-                            <div>
-                                {{ task.description }}
-                            </div>
+                            <h2>{{ task.title }}</h2>
                         </div>
-                        <div class="taskBtn">
-                            <my-button @click="removeTask(task.id)">Delete Task</my-button>
+                        <div>
+                            {{ task.description }}
                         </div>
+                    </div>
+                    <div class="taskBtn">
+                        <my-button @click="removeTask(task.id)">Delete Task</my-button>
                     </div>
                 </div>
             </div>
@@ -104,6 +96,20 @@ import MyButton from '@/UI/myButton.vue';
                 this.taskStore.switchStatus(id, status)
             }
         },
+        computed: {
+            tasksToDo(){
+                return Array.from(this.taskStore.tasks.values()).filter((item) => item.status == 'todo')
+            },
+            tasksInProgress(){
+                return Array.from(this.taskStore.tasks.values()).filter((item) => item.status == 'in-progress')
+            },
+            tasksDone(){
+                return Array.from(this.taskStore.tasks.values()).filter((item) => item.status == 'done')
+            },
+            tasksCancelled(){
+                return Array.from(this.taskStore.tasks.values()).filter((item) => item.status == 'cancelled')
+            },
+        }
     }
 </script>
 
